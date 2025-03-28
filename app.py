d61dd6c21c647cd9ef3fd08d23dac7190936d33f
prity-pat <pritykumarijsr1235@gmail.com>
2025-03-28 11:17:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+import pandas as pd 
+import streamlit as st 
+
+
+df = sns.load_dataset('Titanic')
+
+st.title('Titanic Dataset Visualization')
+sel = st.sidebar.radio('Display DataFrame',('yes','no'))
+
+if sel == 'yes':
+    st.table(df)
+else:
+    st.write('DataFrame not displayed.')    
+
+
+gender =st.sidebar.radio('Select Gender', ('Male','Female'))
+
+if gender == 'Male':
+    male = df[df['sex'] == 'male']
+    fig, ax = plt.subplots()
+    ax = sns.countplot(x = 'class', hue = 'survived', data = male)
+    st.pyplot(fig)
+
+else:
+    female = df[df['sex'] == 'female']
+    fig , ax = plt.subplots()
+    ax = sns.countplot(x = 'class', hue = 'survived', data = female)
+    st.pyplot(fig) 
+
+city =st.sidebar.radio('Select City',('Southampton','Cherbourg','Queenstown'))
+
+if city  == 'Southampton':
+    st.subheader('Southampton - Survival Rate')
+    south = df[df['embark_town'] == 'Southampton']
+    fig, ax = plt.subplots()
+    ax = sns.countplot(x = 'sex' , hue = 'survived' , data = south)
+    st.pyplot(fig)
+
+elif city  == 'Cherbourg':
+    st.subheader('Cherbourg - Survival Rate')
+    south = df[df['embark_town'] == 'Cherbourg']
+    fig, ax = plt.subplots()
+    ax = sns.countplot(x = 'sex' , hue = 'survived' , data = south)
+    st.pyplot(fig)
+
+elif city  == 'Queenstown':
+    st.subheader('Queenstown -Survival rate')
+    south = df[df['embark_town'] == 'Queenstownn']
+    fig, ax = plt.subplots()
+    ax = sns.countplot(x = 'sex' , hue = 'survived' , data = south)
+    st.pyplot(fig)
+
+         
